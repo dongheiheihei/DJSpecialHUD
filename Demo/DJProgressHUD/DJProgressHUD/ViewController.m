d681835ff65c4c147dba58c3258921a25d47f2e1
dongheiheihei <630893613@qq.com>
2020-06-30 14:43:04 +0800

    更更新demo

--- Demo/DJProgressHUD/DJProgressHUD/ViewController.m
+++ Demo/DJProgressHUD/DJProgressHUD/ViewController.m
@@ -27,14 +27,14 @@
     [super viewDidLoad];
     [self showLoading];
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [DJCustomHUD showSuccess:@"恭喜你，成功了！" image:[UIImage imageNamed:@""]];
+        [DJCustomHUD showSuccess:@"恭喜你，成功了！"];
     });
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(4 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [DJCustomHUD showLoading:@"请等待"];
     });
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(7 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [DJCustomHUD hideHUD];
-        [DJCustomHUD showError:@"搞错了" image:[UIImage imageNamed:@""]];
+        [DJCustomHUD showError:@"搞错了"];
     });
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [self hideHUD];
